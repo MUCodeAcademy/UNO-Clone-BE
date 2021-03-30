d5e91027b8bc30cd487cad1de203eb6199db33be
conor-mcgowan <conor.mcgowan@live.com>
2021-03-30 12:13:13 -0500

    Sets up server to use build folder, (dummy build)

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const port = 3000;
 
 app.use(express.static(__dirname + "/build"));
 
-app.get("/", (req, res) => res.send("App go here."));
-// (req, res) => res.sendFile("/build/index.html", { root: __dirname + "/" });
+app.get("/", (req, res) =>
+  res.sendFile("/build/index.html", { root: __dirname + "/" })
+);
 
 app.get("*", (req, res) => res.redirect("/"));
 
