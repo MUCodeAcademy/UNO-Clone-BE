ce72b395a6fb218829eaaae5f21346e228e7ce9c
Joshua Dale <jdale9930@gmail.com>
2021-03-31 11:17:18 -0500

    finished chat functionality

--- server.js
+++ server.js
@@ -1,2 +1,39 @@
 const express = require("express");
 const app = express();
+
+const server = require("http").createServer(app);
+const io = require(`socket.io`)(server, {
+    cors: {
+        origin: "*",
+    },
+})
+
+
+io.on(`connection`, (socket) =>{
+    console.log("connected?")
+    socket.on(`join room`, ({username, room}) =>{
+        console.log("room socket")
+        socket.join(room)
+    })
+    socket.on("message",(msg) =>{
+        // console.log(msg)
+        io.in(msg.room).emit(`message`,{
+            username: msg.username,
+            body: msg.body 
+        })
+    })
+
+        // io.in(socket.room).emit("enter room", {
+        //     username: "SYSTEM", 
+        //     body: `${name} has entered the chat`})
+    
+
+
+    socket.on("disconnect", (name)=>{
+        io.in(socket.room).emit("leave chat", {
+            username: "SYSTEM",
+            body: `${name} has left the chat`})
+    })
+})
+
+server.listen(3001, ()=>{console.log("yeehaw")})
