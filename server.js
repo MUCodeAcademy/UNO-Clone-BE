be1b91f82b04f36581b08733cde39e86d2a30546
Joshua Dale <jdale9930@gmail.com>
2021-04-07 12:24:40 -0500

    fixed double message

--- server.js
+++ server.js
@@ -3,50 +3,48 @@ const app = express();
 const server = require("http").createServer(app);
 const PORT = process.env.PORT || 3001;
 const io = require(`socket.io`)(server, {
-    cors: {
-        origin: "*",
-    },
-})
+  cors: {
+    origin: "*",
+  },
+});
 
-
-io.on(`connection`, (socket) =>{
-    console.log("connected?")
-    socket.on(`join room`, ({username, room}) =>{
-        console.log("room socket")
-        socket.join(room)
-        io.in(room).emit('enter room', {username: username, playerHand: []})
+io.on(`connection`, (socket) => {
+  const { room } = socket.handshake.query;
+  console.log(room);
+  socket.on(`join room`, ({ username }) => {
+    socket.join(room);
+    io.in(room).emit("message",{
+        username: "SYSTEM",
+        body: `${username} has entered the chat`
     })
-    socket.on("message",(msg) =>{
-        io.in(msg.room).emit(`message`,{
-            username: msg.username,
-            body: msg.body 
-        })
-    })
+    io.in(room).emit("enter room", { username: username, playerHand: [] });
+  });
+  socket.on("message", (msg) => {
+    io.in(room).emit(`message`, {
+      username: msg.username,
+      body: msg.body,
+    });
+  });
 
-    socket.on("send player data",(data) =>{
-        io.in(data.room).emit(`host data`, {data})
-    })
-    
-    socket.on("host data send", (data) =>{
-        io.in(data.room).emit(`update game`, {data})
-    })
-    
-        // io.in(socket.room).emit("enter room", {
-        //     username: "SYSTEM", 
-        //     body: `${username} has entered the chat`})
-    
+  socket.on("send player data", (data) => {
+    io.in(room).emit(`host data`, { data });
+  });
 
+  socket.on("host data send", (data) => {
+    io.in(room).emit(`update game`, { data });
+  });
 
-    socket.on("disconnect", (name)=>{
-        io.in(socket.room).emit("leave chat", {
-            username: "SYSTEM",
-            body: `${name} has left the chat`})
-    })
-})
+  // io.in(socket.room).emit("enter room", {
+  //     username: "SYSTEM",
+  //     body: `${username} has entered the chat`})
 
-
-
-
+  socket.on("disconnect", (name) => {
+    io.in(room).emit("leave chat", {
+      username: "SYSTEM",
+      body: `${name} has left the chat`,
+    });
+  });
+});
 
 app.use(express.static(__dirname + "/build"));
 
@@ -54,4 +52,6 @@ app.get("/", (req, res) =>
   res.sendFile("/build/index.html", { root: __dirname + "/" })
 );
 
-server.listen(PORT, ()=>{console.log(`Listening on port: ${PORT}`)})
+server.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
