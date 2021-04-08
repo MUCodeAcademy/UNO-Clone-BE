4aa6879a16d1da71fd7ca14eb5d8f74a881de57f
Mike Truax <mike@miketruax.com>
2021-04-08 14:29:30 -0500

    adds build version

--- build/static/js/main.d5fc1dc0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpuno-front-end"]=this["webpackJsonpuno-front-end"]||[]).push([[0],{11:function(n,t,e){"use strict";e.r(t);var o=e(1),c=e.n(o),r=e(3),s=e.n(r),i=(e(8),e(9),e(0));var u=function(){return Object(i.jsx)("div",{className:"App",children:"Ahoy hoy!"})};s.a.render(Object(i.jsx)(c.a.StrictMode,{children:Object(i.jsx)(u,{})}),document.getElementById("root"))},8:function(n,t,e){},9:function(n,t,e){}},[[11,1,2]]]);
-//# sourceMappingURL=main.d5fc1dc0.chunk.js.map
